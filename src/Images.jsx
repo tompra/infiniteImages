486746815e972a2c7d3d35c8cc0b017c71dfbafc
tompra <tomprayon@gmail.com>
2024-03-11 21:16:40 +0100

    handle add favorite images

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,6 +1,32 @@
+import { useState } from 'react';
 import SingleImage from './SingleImage';
 
-const Images = ({ images, handleAddFavorite }) => {
+const Images = ({ images }) => {
+    const defaultFavList = JSON.parse(
+        localStorage.getItem('favorites') || '[]'
+    );
+    const [listFavorite, setListFavorite] = useState(defaultFavList);
+
+    const handleAddFavorite = (id) => {
+        const addFavorite = images.find((image) => image.id === id);
+        const isAlreadyFavorite = listFavorite.some(
+            (fav) => fav.id === addFavorite.id
+        );
+
+        if (isAlreadyFavorite) {
+            const newFav = listFavorite.filter(
+                (fav) => fav.id !== addFavorite.id
+            );
+            setListFavorite(newFav);
+            localStorage.setItem('favorites', JSON.stringify(newFav));
+            alert('Remove favorite image');
+        } else {
+            const newFav = [...listFavorite, addFavorite];
+            setListFavorite(newFav);
+            localStorage.setItem('favorites', JSON.stringify(newFav));
+            alert('Add favorite image');
+        }
+    };
     return (
         <section className='images-container'>
             {images.map((image) => (
