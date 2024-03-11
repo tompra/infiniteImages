0e7a0dc63688607350567616257e9b15613f5ace
tompra <tomprayon@gmail.com>
2024-03-11 21:19:34 +0100

    use fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,8 @@
-import { useState, useEffect } from 'react';
-import { createClient } from 'pexels';
-import { API_KEY } from '../secret.json';
 import Images from './Images';
-
-const client = createClient(API_KEY);
-const defaultFavList = JSON.parse(localStorage.getItem('favorites') || '[]');
+import useFetchData from './useFetchData';
 
 const App = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-    const [images, setImages] = useState([]);
-    const [page, setPage] = useState(0);
-    const [listFavorite, setListFavorite] = useState(defaultFavList);
-
-    const fetchData = async () => {
-        try {
-            const response = await client.photos.curated({
-                per_page: 40,
-                page: page,
-            });
-
-            setImages(response.photos);
-            setPage(page);
-        } catch (error) {
-            setIsError(true);
-            console.error('Error by fetching images:', error);
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const handleAddFavorite = (id) => {
-        const addFavorite = images.find((image) => image.id === id);
-        const isAlreadyFavorite = listFavorite.some(
-            (fav) => fav.id === addFavorite.id
-        );
-
-        if (isAlreadyFavorite) {
-            const newFav = listFavorite.filter(
-                (fav) => fav.id !== addFavorite.id
-            );
-            setListFavorite(newFav);
-            localStorage.setItem('favorites', JSON.stringify(newFav));
-            alert('Remove favorite image');
-        } else {
-            const newFav = [...listFavorite, addFavorite];
-            setListFavorite(newFav);
-            localStorage.setItem('favorites', JSON.stringify(newFav));
-            alert('Add favorite image');
-        }
-    };
+    const { isLoading, isError, images } = useFetchData();
 
     return (
         <main>
@@ -62,7 +11,7 @@ const App = () => {
             ) : isError ? (
                 <p>Error fetching images. Please try again.</p>
             ) : (
-                <Images images={images} handleAddFavorite={handleAddFavorite} />
+                <Images images={images} />
             )}
         </main>
     );
