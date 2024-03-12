fc7af252d40b96a48d3098d0374d3617181045c1
tompra <tomprayon@gmail.com>
2024-03-12 14:52:56 +0100

    setup lazy images

--- src/LazyImg.jsx
+++ src/LazyImg.jsx
@@ -1,4 +1,30 @@
-const LazyImg = () => {
-    return <div>LazyImg</div>;
+import { useState, useEffect, useRef } from 'react';
+const LazyImg = ({ image }) => {
+    const {
+        src: { original, small, medium, large },
+        alt,
+        photographer,
+    } = image;
+    const [isView, setInView] = useState(false);
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        console.log('LAZY IMAGES EFFECT');
+    }, []);
+
+    return (
+        <>
+            {isView ? (
+                <img
+                    src={original}
+                    srcSet={`${small} 125w, ${medium} 340w, ${large} 940w`}
+                    sizes='(max-width: 125px) 100vw, (max-width: 340px) 100vw, (max-width: 940px) 100vw, 940px'
+                    alt={alt || photographer}
+                />
+            ) : (
+                <div ref={imageRef} style={{ height: '100px' }}></div>
+            )}
+        </>
+    );
 };
 export default LazyImg;
