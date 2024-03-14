eb1164a6d8bba10f098a9b81b142da8c213788ea
tompra <tomprayon@gmail.com>
2024-03-14 19:29:06 +0100

    app testing cases

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,57 +1,61 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import { describe, test, expect, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, test, expect, vi } from 'vitest';
 import App from './App';
-import { fetchDataMock } from './test/fetchDataMock';
+import { initialForty } from './test/imageMock';
+import * as useFetchDataMock from './hooks/fetchDataHook/useFetchData';
+
+const spyFetch = vi.spyOn(useFetchDataMock, 'default');
 
 describe('App component test before fetching data', () => {
-    beforeEach(() => {
+    test('initial render of the app component', () => {
         render(<App />);
     });
-    test('initial render of the app component', () => {});
     test('loading text should appear', async () => {
-        vi.mock('./hooks/fetchDataHook/useFetchData', () => ({
-            __esModule: true,
-            default: vi.fn(() => ({
-                isLoading: true,
-                isError: false,
-                images: [],
-                page: 1,
-                fetchData: vi.fn(),
-            })),
-        }));
+        spyFetch.mockReturnValue({
+            isLoading: true,
+            isError: false,
+            images: [],
+            page: 1,
+            fetchData: vi.fn(),
+        });
 
+        render(<App />);
         expect(screen.getByText('Loading...')).toBeInTheDocument();
     });
-    test('error text should not appear', () => {
-        const errorText = screen.queryByText(
-            'Error fetching images. Please try again.'
-        );
-        expect(errorText).not.toBeInTheDocument();
+    test('error text should appear', () => {
+        spyFetch.mockReturnValue({
+            isLoading: false,
+            isError: true,
+            images: [],
+            page: 1,
+            fetchData: vi.fn(),
+        });
+
+        render(<App />);
+        const errorText = screen.getByTestId('error-message');
+        expect(errorText).toBeInTheDocument();
     });
     test('fetch data should be triggered once at initial render', () => {
+        render(<App />);
+
         const fetchDataMock = vi.fn();
         fetchDataMock();
 
         expect(fetchDataMock).toHaveBeenCalled();
         expect(fetchDataMock).toHaveBeenCalledOnce();
     });
-    test('After fetch data trigger images component should appear', async () => {
+    test('After fetch data trigger images component should appear', () => {
+        spyFetch.mockReturnValue({
+            isLoading: false,
+            isError: false,
+            images: initialForty,
+            page: 1,
+            fetchData: vi.fn(),
+        });
+
         render(<App />);
 
-        vi.mock('./hooks/fetchDataHook/useFetchData', () => ({
-            __esModule: true,
-            default: vi.fn(() => ({
-                isLoading: false,
-                isError: false,
-                images: [fetchDataMock],
-                page: 1,
-                fetchData: vi.fn(),
-            })),
-        }));
-
-        await waitFor(() => {
-            const imagesComponent = screen.getByTestId('images-container');
-            expect(imagesComponent).toBeInTheDocument();
-        });
+        const imagesComponent = screen.getByTestId('images-container');
+        expect(imagesComponent).toBeInTheDocument();
     });
 });
