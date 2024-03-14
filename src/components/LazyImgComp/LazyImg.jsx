5b4451561500f1094575ea68c8a41ca680c4810a
tompra <tomprayon@gmail.com>
2024-03-14 21:56:35 +0100

    remove isView, add data-testid, and add alt attribute placeholder img

--- src/components/LazyImgComp/LazyImg.jsx
+++ src/components/LazyImgComp/LazyImg.jsx
@@ -1,13 +1,12 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-const LazyImg = ({ image }) => {
+const LazyImg = ({ image, isView, setInView }) => {
     const {
         src: { original, small, medium, large },
         alt,
         photographer,
     } = image;
-    const [isView, setInView] = useState(false);
     const imageRef = useRef(null);
 
     useEffect(() => {
@@ -46,11 +45,14 @@ const LazyImg = ({ image }) => {
                     srcSet={`${small} 125w, ${medium} 340w, ${large} 940w`}
                     sizes='(max-width: 125px) 100vw, (max-width: 340px) 100vw, (max-width: 940px) 100vw, 940px'
                     alt={alt || photographer}
+                    data-testid='image'
                 />
             ) : (
                 <img
                     ref={imageRef}
-                    src='https://placehold.co/100x150/000000/FFF?text=Placerholder'
+                    src='https://placehold.co/100x150/000000/FFF?text=Placeholder'
+                    alt='Placeholder'
+                    data-testid='placeholder'
                 />
             )}
         </>
@@ -59,4 +61,6 @@ const LazyImg = ({ image }) => {
 export default LazyImg;
 LazyImg.propTypes = {
     image: PropTypes.object,
+    isView: PropTypes.bool,
+    setInView: PropTypes.func,
 };
