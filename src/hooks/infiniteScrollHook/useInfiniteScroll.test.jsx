025aa8bb41caa7a5a6e8bf66a1e9d9cd656979eb
tompra <tomprayon@gmail.com>
2024-03-16 12:20:52 +0100

    test cases for infinite scroll hook

---
+++ src/hooks/infiniteScrollHook/useInfiniteScroll.test.jsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import { describe, test, expect, vi } from 'vitest';
+import useInfiniteScroll from './useInfiniteScroll';
+import { IntersectionObserverMock } from '../../test/intersectionObserverMock';
+import { getObserverOf, intersect } from '../../test/intersectionObserverMock';
+
+window.IntersectionObserver = IntersectionObserverMock;
+
+const Observe = ({ fetchData }) => {
+    const { observeRef } = useInfiniteScroll(fetchData);
+    return <div data-testid='observeRef' ref={observeRef}></div>;
+};
+
+describe('useInfiniteScroll hook test', () => {
+    const fetchData = vi.fn();
+    test('creates an observer', () => {
+        render(<Observe fetchData={fetchData} />);
+        const observeRef = screen.getByTestId('observeRef');
+        const instance = getObserverOf(observeRef);
+
+        expect(instance.observe).toHaveBeenCalledWith(observeRef);
+    });
+    test('does not call the fetchData function without intersection', () => {
+        render(<Observe fetchData={fetchData} />);
+        const observeRef = screen.getByTestId('observeRef');
+
+        intersect(observeRef, false);
+
+        expect(fetchData).not.toHaveBeenCalled();
+    });
+    test('calls the fetchData function on intersection', () => {
+        render(<Observe fetchData={fetchData} />);
+        const observeRef = screen.getByTestId('observeRef');
+        intersect(observeRef, true);
+
+        expect(fetchData).toHaveBeenCalledTimes(1);
+    });
+});
