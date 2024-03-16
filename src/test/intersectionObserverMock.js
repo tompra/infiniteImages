e6f5f83ff64f8d375fe86723db69b68e272566ec
tompra <tomprayon@gmail.com>
2024-03-16 12:20:18 +0100

    update IntersectionObserver mock and add helper functions

--- src/test/intersectionObserverMock.js
+++ src/test/intersectionObserverMock.js
@@ -1,9 +1,41 @@
 import { vi } from 'vitest';
-const IntersectionObserverMock = vi.fn(() => ({
-    disconnect: vi.fn(),
-    observe: vi.fn(),
-    takeRecords: vi.fn(),
-    unobserve: vi.fn(),
-}));
+
+export class IntersectionObserverMock {
+    constructor(handleObserver) {
+        this.observe = vi.fn((element) => {
+            this.observeElement(element, handleObserver);
+        });
+        this.unobserve = vi.fn((element) => {
+            this.unobserveElement(element);
+        });
+    }
+
+    observeElement(element, handleObserver) {
+        observerMap.set(element, handleObserver);
+        instanceMap.set(element, this);
+    }
+
+    unobserveElement(element) {
+        observerMap.delete(element);
+        instanceMap.delete(element);
+    }
+}
+
+const observerMap = new Map();
+const instanceMap = new Map();
+
+export const intersect = (element, isIntersecting) => {
+    const handleObserver = observerMap.get(element);
+    if (handleObserver) {
+        handleObserver([
+            {
+                isIntersecting,
+                target: element,
+            },
+        ]);
+    }
+};
+
+export const getObserverOf = (element) => instanceMap.get(element);
+
 vi.stubGlobal('IntersectionObserver', IntersectionObserverMock);
-window.IntersectionObserver;
